8e8c9257d8f08613949a6367ea2685bff92da2ab
ClaraPoghosyan <clara.poghosyan@iun.am>
2025-10-27 13:23:38 +0400

    why choose part

--- eduMotive/src/why-choose.component/why-choose.component.ts
+++ eduMotive/src/why-choose.component/why-choose.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-why-choose',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './why-choose.component.html',
   styleUrl: './why-choose.component.scss'
 })
@@ -12,19 +15,19 @@ export class WhyChooseComponent {
       icon: 'assets/icons/puzzle.png',
       title: 'Ճկուն և պահանջով ուսուցում',
       text: 'Մուտք գործիր դասընթացներին ցանկացած ժամանակ, ցանկացած սարքից և սովորիր քեզ հարմար տեմպով։',
-      img: 'assets/flexible-lessons.jpg'
+      img: 'assets/why1.png'
     },
     {
       icon: 'assets/icons/user-trust.png',
       title: 'Փորձառու դասախոսներ',
       text: 'Սովորիր ոլորտի մասնագետներից, ովքեր տրամադրում են իրական գիտելիքներ քո հմտությունները զարգացնելու համար։',
-      img: 'assets/teachers.jpg'
+      img: 'assets/why2.webp'
     },
     {
       icon: 'assets/icons/graduation-cap.png',
       title: 'Մատչելի և հասանելի կրթություն',
       text: 'Վայելիր մատչելի, բարձր որակի դասընթացներ առանց թաքնված վճարների և ստացիր վկայականներ՝ քո կարիերան առաջ տանելու համար։',
-      img: 'assets/money.jpg'
+      img: 'assets/why3.png'
     }
   ];
 }
