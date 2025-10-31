73259ac4038766fa3bcce8f49848060e1cdcdbdf
ClaraPoghosyan <clara.poghosyan@iun.am>
2025-10-31 20:07:07 +0400

    log in

--- eduMotive/src/log-in/log-in.component.ts
+++ eduMotive/src/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from 'ng-zorro-antd/form';
 import {NzInputDirective} from 'ng-zorro-antd/input';
 import {NzCheckboxComponent} from 'ng-zorro-antd/checkbox';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -19,20 +20,31 @@ import {NzButtonComponent} from 'ng-zorro-antd/button';
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit {
+
+  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+  private readonly fb: FormBuilder = inject(FormBuilder);
+
   form: FormGroup;
   isForgotPass: boolean = false;
-  constructor(private fb: FormBuilder) {
+
+  ngOnInit() {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-      remember: [false],
+    });
+
+    this.route.queryParams.subscribe(params => {
+      this.isForgotPass = params['register'] === 'true';
     });
   }
 
-  onSubmit() {
-    if (this.form.valid) {
-      console.log(this.form.value);
+
+  public onSubmit() {
+    if (this.isForgotPass) {
+      console.log('Register flow');
+    } else {
+      console.log('Login flow');
     }
   }
 }
--- eduMotive/src/shared/components/header/header.component.ts
+++ eduMotive/src/shared/components/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NzPageHeaderComponent} from 'ng-zorro-antd/page-header';
 import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro-antd/menu';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
 import {NzHeaderComponent} from 'ng-zorro-antd/layout';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -19,5 +19,9 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private readonly router: Router = inject(Router);
 
+  public goToLogIn(isRegister: boolean): void {
+    this.router.navigate(['/log-in'], { queryParams: { register: isRegister } });
+  }
 }
