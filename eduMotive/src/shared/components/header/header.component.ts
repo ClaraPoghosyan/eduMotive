da0afbc9218f9d3664d27c99ad18c4f9faf1fa2d
ClaraPoghosyan <clara.poghosyan@iun.am>
2025-11-03 18:28:38 +0400

    responsive header

--- eduMotive/src/shared/components/header/header.component.ts
+++ eduMotive/src/shared/components/header/header.component.ts
@@ -4,6 +4,7 @@ import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro
 import {NzButtonComponent} from 'ng-zorro-antd/button';
 import {NzHeaderComponent} from 'ng-zorro-antd/layout';
 import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-header',
@@ -13,15 +14,20 @@ import {Router, RouterLink, RouterLinkActive} from '@angular/router';
     NzButtonComponent,
     NzHeaderComponent,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NzIconDirective
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   private readonly router: Router = inject(Router);
+  public isMenuOpen: boolean = false;
 
   public goToLogIn(isRegister: boolean): void {
     this.router.navigate(['/log-in'], { queryParams: { register: isRegister } });
+  }
+  public closeMenu() {
+    this.isMenuOpen = false;
   }
 }
