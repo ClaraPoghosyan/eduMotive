3fa9a56b6749fbb0cda1350d585bb1676bf2838b
ClaraPoghosyan <clara.poghosyan@iun.am>
2026-02-11 12:22:16 +0400

    courses page be connection

--- eduMotive/src/courses/courses.component.ts
+++ eduMotive/src/courses/courses.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {HeaderComponent} from '../shared/components/header/header.component';
 import {AppFaqComponent} from '../app-faq/app-faq.component';
 import {FooterComponent} from '../footer/footer.component';
 import {JoinPartComponent} from '../join-part.component/join-part.component';
 import {ReviewsComponent} from '../reviews.component/reviews.component';
 import {CoursesConfigComponent} from '../courses-config/courses-config.component';
+import {Course} from '../shared/interfaces/courses.interface';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FaqService} from '../shared/services/faq.service';
+import {CoursesService} from '../shared/services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -17,27 +21,27 @@ import {CoursesConfigComponent} from '../courses-config/courses-config.component
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
-  public featuredCoursesTitle: string= 'Մտքեր, խորհուրդներ հատուկ Ձեզ համար';
+export class CoursesComponent implements OnInit {
+  private readonly destroyRef: DestroyRef = inject(DestroyRef);
+  private readonly coursesService: CoursesService = inject(CoursesService);
+
+  public featuredCoursesTitle: string = 'Ձեր առաջընթացի լավագույն օգնականները';
   public featuredCoursesSubTitle: string = '';
-  public cards = [
-    { title: 'Card title 1', content: 'Card content 1' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 2', content: 'Card content 2' },
-    { title: 'Card title 3', content: 'Card content 3' }
-  ];
+  public courses: Course[] = [];
+
+  ngOnInit() {
+    this.getCourses();
+  }
+
+  public getCourses(): void {
+    this.coursesService.getCourses()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((res: Course[]) => {
+
+        this.courses = res.map(course => ({
+          ...course,
+          isBlog: false
+        }));
+      })
+  }
 }
